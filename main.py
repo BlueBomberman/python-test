87b6e41ebbfe0799c8d8f2b52e0bf432b204dd74
r.pellegrino <r.pellegrino@goreply.io>
2022-01-14 16:44:30 +0100

    fix function bug

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import base64
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
