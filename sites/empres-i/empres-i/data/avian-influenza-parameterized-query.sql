740202c72d7be664c68078bdb2907889992fea1d
r.pellegrino <r.pellegrino@goreply.io>
2022-01-17 16:49:27 +0100

    removed sites folder

--- sites/empres-i/empres-i/data/avian-influenza-parameterized-query.sql
+++
@@ -1,7 +0,0 @@
-SELECT * 
-FROM `fao-empres-re.empres_hih_views.vw_diseases_for_portal`
-where (case when observation_date is null then report_date else observation_date end >= @start_date and case when observation_date is null then report_date else observation_date end <= @end_date)
-  and ( @diagnosis_status = "all" or ifnull(`fao-maps-review.fao_bq_utils.f_normalize_col_name`(diagnosis_status),'no_value') = @diagnosis_status)
-  and ( @animal_type = "all" or ifnull(`fao-maps-review.fao_bq_utils.f_normalize_col_name`(animal_type),'no_value') = @animal_type)
-  and ( disease="Influenza - Avian"); 
-  
